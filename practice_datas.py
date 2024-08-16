e87b9d78d4a05be095e5c394276d65c7c1488679
EAShelenok <e.a.shelenok@mail.ru>
2024-08-16 20:27:50 +1000

    Practice_1_num_and_str

---
+++ practice_datas.py
@@ -0,0 +1,17 @@
+# 1st programm
+print((9 ** 0.5) * 5)
+
+#2nd programm
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd programm
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2 == 2 * (2 + 2))
+
+#4th programm
+print(int(float('123.456') * 10) % 10)
+#or
+a = float('123.456') * 10
+b = int(a)
+print(b % 10)
