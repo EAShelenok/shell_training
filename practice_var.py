d14a36610926cb275f870997390cd1d9b5a46f8b
EAShelenok <e.a.shelenok@mail.ru>
2024-08-23 00:43:24 +1000

    Practice_2_variables

---
+++ practice_var.py
@@ -0,0 +1,7 @@
+done_homework = 12
+hours_spend = 1.5
+course_name = 'Python'
+one_task_hours = hours_spend / done_homework
+print('Курс:', course_name, '; всего задач:', done_homework, ', затрачено часов:', hours_spend, '; среднее время выполнения:', one_task_hours, 'часа.')
+#or more pretty
+print('Курс: ' + course_name + '; всего задач: ' + str(done_homework) + ', затрачено часов: ' + str(hours_spend) + '; среднее время выполнения: ' + str(one_task_hours) + ' часа.')
