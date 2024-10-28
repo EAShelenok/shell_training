ed2cfa333ceb817d01de6f305fd7501fdf132dbd
EAShelenok <e.a.shelenok@mail.ru>
2024-10-28 20:08:38 +1000

    Update ConText.py

--- ConText.py
+++ ConText.py
@@ -1,38 +1,77 @@
 class Context:
-    __n_val = None
 
     def __init__(self):
         self.values = tuple()
 
+    def get_val(self):
+        return self.values
+
     def __enter__(self):
-        self.__n_val = list(self.values)
-        return self.__n_val
+        self.__n = self.values
+        self.values = list(self.values)
+        return self.values
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.values = tuple(self.__n_val)
+        if exc_val is not None:
+            self.values = self.__n
+            raise Exception
+        else:
+            self.values = tuple(self.values)
         return self.values
 
 if __name__ == '__main__':
-
+    
+    #Первый экземпляр
     cm = Context()
     print(cm.values)
-    with cm as list_:
-        list_.append((2.3, 10, 59))
-        list_.append('String')
+    try:
+        with cm as list_:
+            list_.append((2.3, 10, 59))
+            list_.append('String')
+    except Exception:
+        pass
     print(cm.values)
-    with cm as list_1:
-        list_1.append(200)
+    try:
+        with cm as list_:
+            list_.append([200, 'Years'])
+            list_.append(3)
+            list_.append('Class')
+    except Exception:
+        pass
     print(cm.values)
-
+    
+    #Второй экземпляр
     cm_1 = Context()
     print(cm_1.values)
-    with cm_1 as list_:
-        list_.append('New String')
+    try:
+        with cm_1 as list_1:
+            list_1.append('New String')
+            list_1.append(33.6)
+    except Exception:
+        pass
+    print(cm_1.values)
+    try:
+        with cm_1 as list_1:
+            list_1.append('New String_2')
+            list_1.hg(3)
+    except Exception:
+        pass
     print(cm_1.values)
 
+    #Третий экземпляр
     cm_2 = Context()
     print(cm_2.values)
-    with cm_2 as list_:
-        list_.append([[2, 3, 4],'New String 2'])
-        list_.append((45, 30))
+    try:
+        with cm_2 as list_2:
+            list_2.append([[2, 3, 4],'New String 2'])
+            list_2.append((45, 30))
+    except Exception:
+        pass
+    print(cm_2.values)
+    try:
+        with cm_2 as list_2:
+            list_2.scb(3)
+            list_2.append((45, 30))
+    except Exception:
+        pass
     print(cm_2.values)
