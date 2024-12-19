11d5cdba6307b3dde1638d1bd27fd59154592295
EAShelenok <e.a.shelenok@mail.ru>
2024-12-19 19:39:02 +1000

    Practice_53_handmess

---
+++ module_13_2.py
@@ -0,0 +1,18 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = ''
+bot = Bot(token=api)
+di_p = Dispatcher(bot, storage=MemoryStorage())
+
+@di_p.message_handler(commands=['start'])
+async def start(message):
+    print('Привет! Я бот, помогающий твоему здоровью.')
+
+@di_p.message_handler()
+async def all_messages(message):
+    print('Введите команду /start, чтобы начать общение.')
+
+if __name__ == '__main__':
+    executor.start_polling(di_p, skip_updates=True)
